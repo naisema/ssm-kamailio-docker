c7b335d991f6819647be66e24cc28df619f540e7
Suwat Saisema <suwat_sai@hotmail.com>
2021-01-12 03:37:22 +0000

    add dbtext

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -itd --rm -p 5060:5060/udp -p 5060:5060/tcp --name sipproxy kamailio
+docker run --network host --privileged -itd --rm -p 5060:5060/udp -p 5060:5060/tcp --name sipproxy kamailio
